9f21426a233a62023a61d74d1644fee84ddf47ff
Ola Persson <drumpert@gmail.com>
2025-08-19 10:57:32 +0200

    Added ArtWorkDto so I could create Artworks

---
+++ Gallerian.Server/Models/Dtos/ArtWorkDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Gallerian.Server.Models.Dtos
+{
+	public class ArtWorkDto
+	{
+		public string UserId { get; set; }
+		public string Title { get; set; }
+		public string ImageURL { get; set; }
+		public string Description { get; set; }
+		public bool? Private { get; set; }
+		public bool? ForSale { get; set; }
+		public List<int> CategoryIds { get; set; }
+	}
+}
